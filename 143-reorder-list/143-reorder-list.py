a7bc41c433b5d1615cca663fa2f8e5c2b4dcd872
redchicken75 <44732367+redchicken75@users.noreply.github.com>
2022-09-21 00:52:20 +0100

    Time: 210 ms (11.65%), Space: 24 MB (37.23%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        
+        # Use two pointer to find the mid point
+        # Reverse the lower half of the array
+        slow = head
+        fast = head.next
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+            
+        cur, pre, nex = slow.next, None, None
+        slow.next = None
+        while cur != None:
+            nex = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nex
+        
+        
+        # Combine the 2 list
+        firstList = head
+        secondList = pre
+        
+        while secondList:
+            tmp1 = firstList.next
+            tmp2 = secondList.next
+            firstList.next = secondList
+            secondList.next = tmp1
+            firstList, secondList = tmp1, tmp2
+        
+        
+        
+        
+        
+            
+            
