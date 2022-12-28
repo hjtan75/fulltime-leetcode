9519fe4655fbab0977ba234d61c227bcafe662ae
redchicken75 <44732367+redchicken75@users.noreply.github.com>
2022-12-28 19:13:48 +0000

    Time: 243 ms (58.14%), Space: 14.9 MB (41.50%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,28 @@
+class Solution:
+    def binarySearch(self, numbers, num):
+        l = 0
+        r = len(numbers) - 1
+        
+        while l <= r:
+            # print(num, l, r)
+            m = ceil((r + l) / 2)
+            if numbers[m] == num:
+                return m
+            
+            if num > numbers[m]:
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return -1 
+    
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        dic = {}
+        
+        for i in range(int(len(numbers))):
+            t = target - numbers[i]
+            resultIdx = self.binarySearch(numbers, t)
+            if resultIdx >= 0 and i != resultIdx:
+                return sorted([i+1, resultIdx+1])
+            
+            
