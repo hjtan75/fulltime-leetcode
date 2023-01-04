f0c99007bdf7e481620be327e310f6a5c525a14e
redchicken75 <44732367+redchicken75@users.noreply.github.com>
2023-01-04 00:18:51 +0000

    Time: 123 ms (32.08%), Space: 17.6 MB (30.32%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,6 +6,18 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # Traditional method would use a set to keep track of the visited node
+        # But it need O(n) memory complexity
+        # We can reduce it to O(1) with the fast-slow pointer method
+        # The idea is if there exist a loop, the fast pointer will 
+        # meet the slow pointer at some point other than the starting position
+        # This is true because every iteration, the fast pointer increase 2 steps
+        # the slow pointer increase 1 steps, this means that the gap between both
+        # pointer decrease by 1 step.
+        # Thus they will eventually meet
+        # Time complexity: O(n)
+        # Memory complexity: O(1)
+        
         s = head
         f = head
         
