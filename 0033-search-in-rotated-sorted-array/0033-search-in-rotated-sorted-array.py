c46bfa946652222871b4b5898e583f11ff369580
redchicken75 <44732367+redchicken75@users.noreply.github.com>
2023-01-08 21:56:25 +0000

    Time: 38 ms (95.82%), Space: 14.3 MB (54.33%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,38 +1,26 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        lowestPoint = self.findingLowest(nums)
-        nums = nums[lowestPoint:] + nums[:lowestPoint]
-        # print(lowestPoint)
-        # print(newArr)
         l = 0
         r = len(nums) - 1
         
         while l <= r:
-            m = ceil((l+r)/2)
-            if nums[m] == target:
-                return (m+lowestPoint)%len(nums)
-            elif nums[m] > target:
-                r = m - 1
-            else:
-                l = m + 1
-        
-        return -1
-    def findingLowest(self, nums):
-        if nums[0] < nums[-1]:
-            return 0
-        else:
-            l = 0
-            r = len(nums) - 1
+            m = ceil((l + r) / 2)
             
-            while l <= r:
-                m = ceil((l+r)/2)
-                
-                if nums[m] < nums[m-1]:
-                    lowestPoint = m
-                    return m
-                    
-                if nums[m] > nums[0]:
+            if nums[m] == target:
+                return m
+            
+            # Left portion relative to the lowest point
+            if nums[m] >= nums[l]:
+                if target > nums[m] or target < nums[l]:
                     l = m + 1
                 else:
                     r = m - 1
+            # Right portion relative to the lowest point
+            else:
+                if target < nums[m] or target > nums[r]:
+                    r = m - 1
+                else:
+                    l = m + 1
+                    
         return -1
+   
