98e807687bde70eacafcc16d540425d363989fc3
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2023-06-24 14:13:47 +0100

    Time: 54 ms (84.00%), Space: 16.9 MB (23.20%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,12 +1,37 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        ci, cj = 0, len(matrix) - 1
+        # Find which row that main conatain the integer
+        # From each row perform binary search
         
-        while ci < len(matrix[0]) and cj >= 0:
-            if matrix[cj][ci] == target:
-                return True
-            elif matrix[cj][ci] > target:
-                cj -= 1
+        top, bot = 0, len(matrix) - 1
+        midRow = 0
+        while top <= bot:
+            midRow = int((bot + top) / 2)
+            # print(top, bot)
+            if matrix[midRow][-1] >= target and matrix[midRow][0] <= target:
+                break
+            elif matrix[midRow][0] > target:
+                bot = midRow - 1
             else:
-                ci += 1
+                top = midRow + 1
+                
+        if top > bot:
+            return False
+        # print(midRow)
+        l, r  = 0, len(matrix[midRow]) - 1
+        while l <= r:
+            midCol = int((l + r) / 2)
+            if matrix[midRow][midCol] == target:
+                return True
+            elif matrix[midRow][midCol] > target:
+                r = midCol - 1
+            else:
+                l = midCol + 1
+                
         return False
+
+        
+                
+                
+        
+                
