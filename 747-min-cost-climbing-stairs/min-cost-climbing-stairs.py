134984e2908213ba35537614d8e5f843930852c2
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2023-11-28 11:51:47 +0000

    Time: 59 ms (68.94%) | Memory: 16.5 MB (37.59%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -3,10 +3,13 @@ class Solution:
         nums_steps = len(cost)
 
         cost1 = cost[:2]
+        c1, c2 = cost[0], cost[1]
 
-        while len(cost1) < nums_steps:
-            prev_min = min(cost1[-1], cost1[-2])
-            cost1.append(cost[len(cost1)] + prev_min)
+        # while len(cost1) < nums_steps:
+        #     prev_min = min(cost1[-1], cost1[-2])
+        #     cost1.append(cost[len(cost1)] + prev_min)
 
-        # print(cost1)
-        return min(cost1[-1], cost1[-2])
+        for i in range(2, nums_steps):
+            c1, c2 = c2, min(c1, c2) + cost[i]
+
+        return min(c1, c2)
