0b2fd6271a22848a6ea895aee641210d3cc3e119
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2023-12-08 01:38:17 +0000

    Time: 37 ms (99.64%) | Memory: 16.5 MB (9.88%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,34 +1,55 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = set()
+        candidates.sort()
+        res = []
+        comb = []
         def dfs(val, s_idx):
             if val < 0:
                 return
             
             if val == 0:
-                str_com = map(str, sorted(combination))
-                str_com = " ".join(str_com)
-                res.add(str_com)
+                res.append(comb.copy())
                 return
 
             prev = -1
-            for i in range(s_idx+1, len(candidates)):
+            for i in range(s_idx, len(candidates)):
                 if prev == candidates[i]:
                     continue
-                combination.append(candidates[i])
-                dfs(val - candidates[i], i)
-                combination.pop()
+                if val - candidates[i] < 0:
+                    break
+                comb.append(candidates[i])
+                dfs(val - candidates[i], i+1)
+                comb.pop()
                 prev = candidates[i]
 
-    
-        for i in range(len(candidates)):
-            combination = [candidates[i]]
-            dfs(target - candidates[i], i)
 
-        list_ans = []
-        while res:
-            str_com = res.pop()
-            str_com = str_com.split(" ")
-            list_ans.append(map(int, str_com))
-        return list_ans
+        dfs(target, 0)
+        return res
+
+'''
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+
+        res = []
+
+        def backtrack(cur, pos, target):
+            if target == 0:
+                res.append(cur.copy())
+                return
+            if target <= 0:
+                return
+
+            prev = -1
+            for i in range(pos, len(candidates)):
+                if candidates[i] == prev:
+                    continue
+                cur.append(candidates[i])
+                backtrack(cur, i + 1, target - candidates[i])
+                cur.pop()
+                prev = candidates[i]
+
+        backtrack([], 0, target)
+        return res
+'''
                 
