353977c613936bc6704ff9a50b1d32cff74532e9
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2023-12-08 12:25:56 +0000

    Time: 565 ms (30.88%) | Memory: 33.8 MB (18.40%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,27 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res, par = [], []
+
+        def dfs(idx):
+            if idx == len(s):
+                res.append(par.copy())
+                return
+
+            for j in range(idx, len(s)):
+                if isPalin(idx, j):
+                    par.append(s[idx:j+1])
+                    dfs(j+1)
+                    par.pop()
+
+        def isPalin(l, r):
+            while l <= r:
+                if s[l] != s[r]:
+                    return False
+
+                l += 1
+                r -= 1
+        
+            return True
+
+        dfs(0)
+        return res
