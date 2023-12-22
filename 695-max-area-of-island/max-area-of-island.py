c46d058887ec0fa6c9f6272a06b6649c3da294e5
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2023-12-22 20:35:37 +0000

    Time: 97 ms (99.81%) | Memory: 17.5 MB (72.78%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.py
@@ -0,0 +1,46 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        # O(n * )
+        n_row = len(grid)
+        n_col = len(grid[0])
+        parent = [-1 for i in range(n_row * n_col)]
+        size = [0 for i in range(n_row * n_col)]
+
+        def find(a):
+            # O(n)
+            while parent[a] != a:
+                a = parent[a]
+            return a
+
+        def union(a, b):
+            # O(2n)
+            parent_a = find(a)
+            parent_b = find(b)
+
+            if parent_a != parent_b:
+                parent[parent_a] = parent_b
+                size[parent_b] = size[parent_b] + size[parent_a] 
+            
+        def cell_parsing():
+            for i in range(n_row):
+                for j in range(n_col):
+                    if grid[i][j] == 1:
+                        idx = i*n_col + j
+                        parent[idx] = idx
+                        size[idx] = 1
+                        if i != 0 and grid[i-1][j] == 1:
+                                union(idx, idx - n_col)
+
+                        if j != 0 and grid[i][j-1] == 1:
+                                union(idx, idx - 1)
+
+            max_size = 0
+            # print(parent)
+            # print(size)
+            for i in range(len(parent)):
+                if i == parent[i] and size[i] > max_size:
+                    max_size = size[i]
+
+            return max_size
+
+        return cell_parsing()
