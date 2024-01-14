d4bfd5b359f3bd1573f422f865ba74eb665ee091
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-01-14 23:00:47 +0000

    Time: 213 ms (35.20%) | Memory: 18.5 MB (21.79%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -0,0 +1,37 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        # Time: O(n log n)
+        # Memory: O(n)
+        # Operation is just swapping character, so need to record number of occurrance
+        # Record the number of occurance for two strings
+        # Sort two array by the number of occurance
+        # if both array are the same then return true else false
+
+        if len(word1) != len(word2):
+            return False
+        occur1 = [0] * 26
+        occur2 = [0] * 26
+
+        for c in word1:
+            asci = ord(c) - ord('a')
+            occur1[asci] += 1
+
+        for c in word2:
+            asci = ord(c) - ord('a')
+            occur2[asci] += 1
+
+        for i in range(len(occur1)):
+            # if (occur1[i] == 0 and occur2[i] != 0) or (occur1[i] != 0 and occur2[i] == 0):
+            if (occur1[i] == 0) ^ (occur2[i] == 0):
+                return False
+
+        occur1.sort()
+        occur2.sort()
+
+        for i in range(len(occur1)):
+            if occur1[i] != occur2[i]:
+                return False
+
+        return True
+
+        
