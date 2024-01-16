a418dae3a99c94887f3a6542c264328e95de595d
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-01-16 15:46:16 +0000

    Time: 254 ms (94.11%) | Memory: 64.5 MB (32.95%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -4,29 +4,36 @@ class RandomizedSet:
     # To remove and element, we know the index of the element
     # but to pop at that index, it will take O(n) because of the shift action
     # We could swap the element with the last element in the array, so that
+    # pop only require O(1)
+    # Have to update the dictionary of the new location.
     def __init__(self):
-        self.this_set = set()
+        self.dict = {}
+        self.arr = []
 
     def insert(self, val: int) -> bool:
-        if val in self.this_set:
+        if val in self.dict:
             return False
         else:
-            self.this_set.add(val)
+            self.arr.append(val)
+            self.dict[val] = len(self.arr) - 1
             return True
 
 
     def remove(self, val: int) -> bool:
-        if val not in self.this_set:
+        if val not in self.dict:
             return False
         else:
-            self.this_set.remove(val)
+            idx_to_be_remove = self.dict[val]
+            val_to_be_swapped = self.arr[-1]
+            self.arr[idx_to_be_remove], self.arr[-1] = self.arr[-1], self.arr[idx_to_be_remove]
+            self.arr.pop()
+            self.dict[val_to_be_swapped] = idx_to_be_remove
+            del self.dict[val]
             return True
         
 
     def getRandom(self) -> int:
-        arr = list(self.this_set)
-        rand_int = random.randint(0, len(arr)-1)
-        return arr[rand_int]
+        return random.choice(self.arr)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
