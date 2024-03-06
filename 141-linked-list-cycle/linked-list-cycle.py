a0c3865234c17d976b949c23b6b4c8fd033bfe4e
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-03-06 17:03:57 +0000

    Time: 48 ms (41.34%) | Memory: 18.9 MB (96.32%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,14 +6,22 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        visited = []
+        # There is a solution where two pointer is used
+        # One fast pointer and the other one is slow
+        # If the fast pointer meet the slow pointer, there is a loop
+        # If the fast pointer reachers the end before meeting the slow pointer
+        # There is not loop
+        # Time: O()
+        # Memory: O(1)
+        if head == None or head.next == None:
+            return False
 
-        ptr = head
-        while ptr:
-            if ptr in visited:
+        slow, fast = head, head.next
+
+        while fast.next and fast.next.next:
+            if slow == fast:
                 return True
-            visited.append(ptr)
-            ptr = ptr.next
+            slow = slow.next
+            fast = fast.next.next
 
         return False
-        
