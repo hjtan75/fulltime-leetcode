1063541feed8e072276e2f0b68a514c8c41167fc
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-03-26 23:25:02 +0000

    Time: 288 ms (67.11%) | Memory: 30.4 MB (55.56%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,19 +1,23 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        # This places all element on the respective array column
+        # We don't have to consider number > n because there will certain that a smaller
+        # number exist before that.
+        # TC: O(2n), one for putting every element to the respective place
+        # The other used to parse the array for smallest integer
+        # MC: O(1), because we only uses the original array
+        # Swap function is compulsary because python funciton is pass by value, so the value could be sway
+        # If not, python list is immutable, so the value don't change
         def swap(arr, i, j):
             arr[i], arr[j] = arr[j], arr[i]
-        
+
         n = len(nums)
-        
-        # Place each positive integer i at index i-1 if possible
+
         for i in range(n):
             while 0 < nums[i] <= n and nums[i] != nums[nums[i] - 1]:
                 swap(nums, i, nums[i] - 1)
-        
-        # Find the first missing positive integer
         for i in range(n):
             if nums[i] != i + 1:
                 return i + 1
-        
-        # If all positive integers from 1 to n are present, return n + 1
+
         return n + 1
