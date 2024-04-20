a23e571ce7cbc36fdd01c7237116add312c8824c
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-04-20 15:40:02 +0100

    Time: 878 ms (50.00%) | Memory: 30 MB (80.50%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
@@ -1,37 +1,53 @@
 class Solution:
     def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
+        # Approach 1
         # Use counting method, if a farmland is encountered, heck it's upper and leftsidr
         # If those are not farmland, it's a new start of farmland, marked as new farmland
         # Add the starting position to hashmap and mark it as visited
         # if those are farmland, see what's the original starting position based on their visited value,
         # update the last postion in hashmap
+        
+        # Appraoch 2
+        # apprach to dfs
+        # One farmland is encountered, explore it using dfs
+        # until the last corner is found
 
         n, m = len(land), len(land[0])
-        marking = {}
-        visited = [[[0, 0] for _ in range(m)] for _ in range(n)]
+        visited = [[False for k in range(m)] for j in range(n)]
+        res = []
 
+        def explore(i, j):
+            if visited[i][j] or land[i][j] == 0:
+                return None
+            
+            r_1, c_1 =  i, j
+            r_2, c_2 = dfs(i, j)
+            return [r_1, c_1, r_2, c_2]
+            
+
+        def dfs(i, j):
+            if visited[i][j] or land[i][j] == 0:
+                return None
+            
+            visited[i][j] = True
+            d1, d2 = None, None
+            if i < n-1:
+                d1 = dfs(i+1, j)
+            if j < m-1:
+                d2 = dfs(i, j+1)
+
+            if not d1 and not d2:
+                return [i, j]
+            elif d1 == None:
+                return d2
+            else:
+                return d1
+            
         for i in range(n):
             for j in range(m):
-                if land[i][j] == 1:
-                    if i > 0 and land[i-1][j] == 1:
-                        s_i, s_j = visited[i-1][j][0], visited[i-1][j][1]
-                        visited[i][j][0] = s_i
-                        visited[i][j][1] = s_j
-                        marking["{} {}".format(s_i, s_j)] = "{} {}".format(i, j)
-                    elif j > 0 and land[i][j-1] == 1:
-                        s_i, s_j = visited[i][j-1][0], visited[i][j-1][1]
-                        visited[i][j][0] = s_i
-                        visited[i][j][1] = s_j
-                        marking["{} {}".format(s_i, s_j)] = "{} {}".format(i, j)
-                    else:
-                        visited[i][j][0] = i
-                        visited[i][j][1] = j
-                        print(i, j)
-                        marking["{} {}".format(i, j)] = "{} {}".format(i, j)
-        res = []
-        for k, v in marking.items():
-            r_1, c_1 = map(int, k.split(" "))
-            r_2, c_2 = map(int, v.split(" "))
-            res.append([r_1, c_1, r_2, c_2])
+                coord = explore(i, j)
+                if coord != None:
+                    res.append(coord)
 
         return res
+
