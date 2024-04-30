5c7ade94643a997d8ec1de2561a64da68e3fd7b4
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-04-30 17:02:05 +0100

    Time: 1121 ms (77.89%) | Memory: 17.7 MB (22.12%) - LeetSync

--- 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.py
+++ 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.py
@@ -1,16 +1,24 @@
-class Solution(object):
-    def wonderfulSubstrings(self, word):
-        count = [0] * 1024  # 2^10 to store XOR values
-        result = 0
-        prefix_xor = 0
-        count[prefix_xor] = 1
-        
-        for char in word:
-            char_index = ord(char) - ord('a')
-            prefix_xor ^= 1 << char_index
-            result += count[prefix_xor]
+class Solution:
+    def wonderfulSubstrings(self, word: str) -> int:
+        # We use bitmask for this task
+        # Because the character is from a to j, we can represent it from 0000 0000 00 to 1000 0000 00
+        # For each character, we add it into the prefix, we can do that with xor operation
+        # For a substring bit mask is the same as the entire string, means that that is a even wonderful 
+        # We need to consider scenario where at most odd, we flip a single bit on the bit mask then check
+        # Whether it has been encountered before
+
+        count = [0] * 1024 # Count the frequency of a bitmask
+        res = 0
+        bitmask = 0
+        count[0] = 1 # We set the empty substring to be 1 because this mean the string itself is wonderfull
+
+        for c in word:
+            bitmask ^= (1<<ord(c) - ord('a'))
+            # print("-", bitmask)
+            res += count[bitmask]
             for i in range(10):
-                result += count[prefix_xor ^ (1 << i)]
-            count[prefix_xor] += 1
-        
-        return result
+                res += count[bitmask ^ (1<<i)]
+                # print(bitmask ^ (1<<i))
+            count[bitmask] += 1
+
+        return res
