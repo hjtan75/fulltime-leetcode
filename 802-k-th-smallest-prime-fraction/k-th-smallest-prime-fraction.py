d09b7d67817dba52e73ca779a7e90945155cbae3
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-05-10 23:01:01 +0100

    Time: 71 ms (86.39%) | Memory: 16.8 MB (90.48%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
@@ -3,14 +3,32 @@ class Solution:
         # I think this will be a two pointer question
         # The fact that the array in sorted make it that we don't have to solve it in try
         # Every possibility
-
-        ans = []
         n = len(arr)
+        low, high = 0, 1.0
 
-        for i in range(n):
-            for j in range(i+1, n):
-                ans.append((arr[i]/arr[j], [arr[i], arr[j]]))
+        while low < high:
+            mid = (low + high) / 2
+            count, num, den = self.helper(arr, mid)
 
-        ans.sort()
-        # print(list(ans[k])[1])
-        return list(ans[k-1])[1]
+            if k < count:
+                high = mid
+            elif k > count:
+                low = mid
+            else:
+                return [num, den]
+
+        return []
+
+    def helper(self, arr, target):
+        count, i, n = 0, 0, len(arr)
+        num, den = arr[0], arr[n - 1]
+
+        for j in range(1, n):
+            while i < j and arr[i] <= arr[j] * target:
+                i += 1
+            count += i
+            if i > 0 and arr[i - 1] * den > arr[j] * num:
+                num = arr[i - 1]
+                den = arr[j]
+
+        return [count, num, den]
