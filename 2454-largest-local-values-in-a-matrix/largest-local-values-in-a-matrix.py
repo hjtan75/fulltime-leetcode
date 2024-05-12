8acd1f10a48d09f9f4ae0b81bf901eeab0f2cb26
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-05-12 15:28:32 +0100

    Time: 118 ms (76.18%) | Memory: 17.1 MB (28.98%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
@@ -1,17 +1,33 @@
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        # For every new column added, we find the largest element in the row
+        # add that to the set and remove the previous largest element in the set, 
+        # find the largest element in the set
+        # Instead of O(3n) to O(n)
+
         n, m = len(grid), len(grid[0])
         res = [[0 for _ in range(m-2)] for _ in range(n-2)]
 
         for i in range(n-2):
-            for j in range(m-2):
-                maxi = 0
+            prev = 0
+            col_max_set = deque([])  
+            for j_sq in range(3):
+                col_max = 0
                 for i_sq in range(3):
-                    for j_sq in range(3):
-                        maxi = max(maxi, grid[i+i_sq][j+j_sq])
+                    col_max = max(col_max, grid[i+i_sq][j_sq])
+                col_max_set.append(col_max)
 
-                res[i][j] = maxi
+            res[i][0] = max(col_max_set)
+            for j in range(3, m):
+                col_max = 0
+                for i_sq in range(3):
+                    col_max = max(col_max, grid[i+i_sq][j])
+                
+                col_max_set.append(col_max)
+                col_max_set.popleft()
+                res[i][j-2] = max(col_max_set)
 
         return res
+
 
             
