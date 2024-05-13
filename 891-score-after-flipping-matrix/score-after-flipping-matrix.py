01e129f416739312a78e9271abd91679947e9444
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-05-13 17:44:18 +0100

    Time: 45 ms (28.02%) | Memory: 16.6 MB (18.53%) - LeetSync

---
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.py
@@ -0,0 +1,44 @@
+class Solution:
+    def matrixScore(self, grid: List[List[int]]) -> int:
+        # We can infinate moves,
+        # The aim for row toggle, if to get as many 1 in the front as possible
+        # The aim for col toggle, is to generate more 1 in the entire col
+        # But which action do we carry out first? row or col?
+        # We do row toggle first, because col will only increase the final value
+
+        n, m = len(grid), len(grid[0])
+        row_mask = 0
+
+        for _ in range(m):
+            row_mask <<= 1
+            row_mask += 1
+
+        for i in range(n):
+            num = int(''.join(map(str, grid[i])), 2)
+            nega = num ^ row_mask
+
+            # print(num, nega)
+
+            if nega > num:
+                for j in range(m):
+                    grid[i][j] = 1 - grid[i][j]
+
+        # print(grid)
+        for j in range(m):
+            one_cnt = 0
+            for i in range(n):
+                if grid[i][j] == 1:
+                    one_cnt += 1
+                else:
+                    one_cnt -= 1
+
+            if one_cnt < 0:
+                for i in range(n):
+                    grid[i][j] = 1 - grid[i][j]
+
+        # print(grid)
+        res = 0
+        for i in range(n):
+            res += int(''.join(map(str, grid[i])), 2)
+
+        return res
