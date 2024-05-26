9c4d03369800d1fe29661b8f5a662c081440a338
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-05-27 00:10:08 +0100

    Time: 116 ms (96.10%) | Memory: 36 MB (61.46%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.py
+++ 552-student-attendance-record-ii/student-attendance-record-ii.py
@@ -1,29 +1,24 @@
 class Solution:
     def checkRecord(self, n: int) -> int:
-        # the naive method would be to try out all possiblities 
-        # The way to optimize is too cancel the branch when is has to possiblity to be added
-        # This is a dynamic programming question
-        # TC: O(3^(n-2) + )
-        mod = 10**9 + 7
-        dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(n+1)]
+        import numpy as np
 
-        # print(dp)
+        T = np.array([[1, 1, 0, 1, 0, 0],
+                      [1, 0, 1, 1, 0, 0],
+                      [1, 0, 0, 1, 0, 0],
+                      [0, 0, 0, 1, 1, 0],
+                      [0, 0, 0, 1, 0, 1],
+                      [0, 0, 0, 1, 0, 0]])
 
-        def dfs(idx, absent, late):
-            if absent >= 2 or late >= 3:
-                return 0 
+        def matPow(A, p):
+            if p == 1:
+                return A
+            if p % 2 == 0:
+                B = matPow(A, p // 2)
+                return (B @ B) % (10**9 + 7)
+            else:
+                return (A @ matPow(A, p-1)) % (10**9 + 7)
 
-            if idx == n:
-                return 1
+        A = matPow(T, n)
 
-            if dp[idx][absent][late] != -1:
-                return dp[idx][absent][late]
+        return int(sum(A[0]) % (10**9 + 7))
 
-            ans = dfs(idx+1, absent, 0)
-            ans += dfs(idx+1, absent+1, 0)
-            ans += dfs(idx+1, absent, late+1)
-
-            dp[idx][absent][late] = ans % mod
-            return dp[idx][absent][late]
-
-        return dfs(0, 0, 0)
