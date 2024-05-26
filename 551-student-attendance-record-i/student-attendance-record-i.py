c69d7582f24f3cb896b39a285822b9ea1966b0f2
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-05-26 22:27:35 +0100

    Time: 28 ms (91.11%) | Memory: 16.3 MB (99.57%) - LeetSync

--- 551-student-attendance-record-i/student-attendance-record-i.py
+++ 551-student-attendance-record-i/student-attendance-record-i.py
@@ -2,22 +2,15 @@ class Solution:
     def checkRecord(self, s: str) -> bool:
         con_late = 0
         absent = 0
-        prev_late = False
 
         for c in s:
             if c == 'A':
                 absent += 1
-                prev_late = False
-
-            if c == 'P':
-                prev_late = False
             
             if c == 'L':
-                if prev_late:
-                    con_late += 1
-                else:
-                    con_late = 1
-                prev_late = True
+                con_late += 1
+            else:
+                con_late = 0
 
             if absent >= 2 or con_late >= 3:
                 return False
