abb94e5070f1fa2457c51e500283af4a6d691fdc
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-05-31 18:02:16 +0100

    Time: 44 ms (18.27%) | Memory: 16.8 MB (13.13%) - LeetSync

---
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        # because we could get the parent of the deleted node
+        # we could only change the value of the current node to the next node
+        # then delete the last node
+
+        prev = None
+        while node.next:
+            node.val = node.next.val
+            prev = node
+            node = node.next
+
+        prev.next = None
+
+        
