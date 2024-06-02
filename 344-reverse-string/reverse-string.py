0e37963dacaec46a030fffa4ca14ff5f4b4598a7
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-06-02 15:36:47 +0100

    Time: 166 ms (72.16%) | Memory: 21 MB (45.58%) - LeetSync

---
+++ 344-reverse-string/reverse-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # Not extra memory, so we have two pointers
+        # One pointed to the first element and one to the last
+        # We swap the value between those to cells and the increament the start
+        # pointer and decreasement the last pointer until start <= end
+
+        start, end = 0, len(s) - 1
+
+        while start < end:
+            s[start], s[end] = s[end], s[start]
+            start += 1
+            end -= 1
+
+        
