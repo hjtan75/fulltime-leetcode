d396be533039a20a5102ae0cba7a5b3bf9f5d768
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-06-04 00:13:55 +0100

    Time: 56 ms (11.62%) | Memory: 16.6 MB (62.61%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -8,32 +8,10 @@ class Solution:
 
         maping = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         res = 0
-        for i in range(len(s) - 1):
-            if s[i] == 'I':
-                if (s[i+1] == 'V' or s[i+1] == 'X'):
-                    print('bug')
-                    res -= 1
-                else:
-                    res += 1
-            elif s[i] == 'V':
-                res += 5
-            elif s[i] == 'X':
-                if (s[i+1] == 'L' or s[i+1] == 'C'):
-                    res -= 10
-                else:
-                    res += 10
-            elif s[i] == 'L':
-                res += 50
-            elif s[i] == 'C':
-                if (s[i+1] == 'D' or s[i+1] == 'M'):
-                    res -= 100
-                else:
-                    res += 100
-            elif s[i] == 'D':
-                res += 500
+        for i in range(len(s)):
+            if i < len(s) - 1 and maping[s[i]] < maping[s[i+1]]:
+                res -= maping[s[i]]
             else:
-                res += 1000
-
-        res += maping[s[-1]]
+                res += maping[s[i]]
 
         return res
