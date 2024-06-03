61f4217de960b94a939ca3848c13c0e9456caf15
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-06-03 17:18:45 +0100

    Time: 197 ms (46.36%) | Memory: 30.3 MB (59.09%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        # The brute foce method would be to list every possible array
+        # And the sum the element in the array to find out whether the sum is greater 
+        # then the target
+        # O(n^2) because we are listing out all the possible subarray
+
+        # A rolling window, start and end are pointed at the start of the array
+        # If we sum is smaller than the target, increament end
+        # if sum is larger than the target, increasement start
+        # if sum is equal, increament both indexes,
+        # Edge cases: if not possible to sum up to target with in the array, return 0
+        # TC: O(n)
+
+        if sum(nums) < target:
+            return 0
+
+        start, end = 0, 0
+        summ = 0
+        min_length = len(nums)
+
+        while start < len(nums) and end < len(nums):
+            while end < len(nums) and summ < target:
+                summ += nums[end]
+                end += 1
+
+            while start < end and summ >= target:
+                min_length = min(min_length, end - start)
+                summ -= nums[start]
+                start += 1
+            
+        return min_length
+
+            
