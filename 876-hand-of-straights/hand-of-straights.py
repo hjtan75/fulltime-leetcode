683cd3dd5f71b930762bf9feca6863e384f503a8
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-06-06 21:55:51 +0100

    Time: 162 ms (60.62%) | Memory: 18.4 MB (64.12%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -3,44 +3,50 @@ class Solution:
         # First we have to check if whether len(hand) % group size == 0,
         # if not return false, we have to the array
 
-        # We can sort the array, and we need to find the trasition point in the array
-        # we parse throught the array and add it to and stack.
-        # we only add to stack when hand[0] == stack[i] + 1
-        # if stack == groupsize, clear stack, if hand[-1] > stack[-1] + 1, return false
-        # if hand[-1] <= stack[i] , i -=1
-        # TC: O(n^2), that is worst case which will never happend, because in that case,
-        # the function would have return false on the first run, is there is a solution
-        # Imporve by binary search log n
-        # TC: O()
-        # SC: O(n), which can be optimized, we can have one variable marking the size and 
-        # the other marking the value of the last element in the stack
+        # We use a hashmap to store the frequency of each number in hand
+        # Then we sort all the distinct number in the array
+        # first we take a number from the front of the array, then find weather
+        # val + 1 is in the hashmap, if yes decrement, if not return false
+        # if hashmap decreament until 0, remove from the array
+        # O(n log n) for sorting
+        # SP: O(n)
+
         if len(hand) % groupSize != 0:
             return False
 
-        hand = sorted(hand)
-        while len(hand) > 0:
-            stack_len, stack_val = 1, hand[0]
-            del hand[0]
-            while stack_len < groupSize:
-                target = stack_val + 1
-                l, r = 0, len(hand) - 1
-                while l <= r:
-                    m = (l+r) // 2
-                    if hand[m] == target:
-                        stack_val = target
-                        stack_len += 1
-                        del hand[m]
-                        break
-                    elif hand[m] > target:
-                        r = m - 1
-                    else:
-                        l = m + 1
+        counter = {}
+        distinct_arr = []
+        for card in hand:
+            if card in counter:
+                counter[card] += 1
+            else:
+                counter[card] = 1
+                distinct_arr.append(card)
 
-                # Can't find target
-                if l > r:
-                    return False
+        distinct_arr = sorted(distinct_arr, reverse=True)
+        while len(distinct_arr) > 0:
+            stack_len, stack_val = 0, 0
+            while stack_len < groupSize:
+                if stack_len == 0:
+                    if len(distinct_arr) == 0:
+                        return False
+                    stack_val = distinct_arr[-1]
+                else:
+                    if (stack_val + 1) not in counter or counter[stack_val + 1] == 0:
+                        return False
+                    else:
+                        stack_val += 1
+
+                counter[stack_val] -= 1
+                stack_len += 1
+                if counter[stack_val] == 0:
+                    distinct_arr.pop()
 
         return True
+
+
+
+        
 
 
 
