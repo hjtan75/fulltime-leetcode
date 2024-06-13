d555120535b623faffebf983e3d59dd0669d82b3
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-06-13 16:32:34 +0100

    Time: 38 ms (35.08%) | Memory: 16.5 MB (43.45%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,16 +1,26 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        alpha_count = {}
+        # Because all character in order is unique
+        # we can count the character is s and rearrange them in a way that it follows the 
+        # order, because s might be bigger than order, so if order is if and s is ifif
+        # should be rearragne to iiff, 
+        # after appending all the character in order, we append the rest of the character of s
+        # Tc: O(n) because we parse though s one, anad because order is limit by 26, so it's O(1)
+        # SP: O(1) because character is bound by 26
+
+        counter = {}
         res = []
-        for alpha in s:
-            alpha_count[alpha] = alpha_count.get(alpha, 0) + 1
 
+        for c in s:
+            counter[c] = counter.get(c, 0) + 1
 
-        for alpha in order:
-            res.append(alpha * alpha_count.pop(alpha, 0))
+        for c in order:
+            if c in counter:
+                res.append(''.join([c for _ in range(counter[c])]))
+                counter[c] = 0
 
-        for k, v in alpha_count.items():
-            for _ in range(v):
-                res.append(k)
+        for cha, occ in counter.items():
+            if occ > 0:
+                res.append(''.join([cha for _ in range(occ)]))
 
         return ''.join(res)
