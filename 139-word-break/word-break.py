c5e8b868e8da8a3a8d1cfbeabe2700cdd1bc1602
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-06-13 13:43:17 +0100

    Time: 41 ms (50.91%) | Memory: 16.7 MB (54.37%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,20 +1,28 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # The brute force method would be trying every wordDict for each char in s
-        # TC: O(m^n) n: number character in s and m: length of wordDict
+        # The brute force method is to test every character in the dictionary
+        # THe time complexity wil be O(nmc), n is the number of character in s
+        # m is the length of wordDict and c is the length of word in dict
 
-        # This is a dynamic prograaming question, so i should store the previous result
-        
-       
-        n = len(s)
-        dp = [False] * (n+1)
-        dp[0] = True
+        # This is a dynamic programming question
+        # Got the save the previous state in dp
+        # dp is false for all element i, and only true when element[i] could be represent 
+        # as a segmentation of dictionary
+        # so for very element in dictioary we check wheather it exist in s[i:i+w]
+        # If it does mark i+w as true
+        # Tc: O(nm) n == character in s and m is the length of dictionary
 
-        for i in range(n):
+        dp = [False] * (len(s) + 1)
+
+        i = 0
+        dp[i] = True
+        while i < len(s):
             if dp[i]:
-                for w in wordDict:
-                    if i + len(w) <= n and s[i:i+len(w)] == w:
-                        dp[i + len(w)] = dp[i]
-            if dp[n]:
-                return True
-        return False
+                for word in wordDict:
+                    if i + len(word) <= len(s) and not dp[i+len(word)] and s[i:i+len(word)] == word:
+                        dp[i+len(word)] = True
+
+            i += 1
+
+        print(dp)
+        return dp[-1]
