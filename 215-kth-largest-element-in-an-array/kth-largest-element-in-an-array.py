747538cc32b90eef84e68f012f6f8af4415b7602
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-06-17 10:30:57 +0100

    Time: 498 ms (22.26%) | Memory: 28.7 MB (92.37%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,23 +1,21 @@
+import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def quickselect(start, end, arr, k):
-            pivot_value = arr[end]
-            pivot_point = start
+        # quick select is a method that could find kth largest element in avearge O(n)
+        # the worst case for quick select would be O(n^2)
+        # the easy way is to use sorting which require O(n log n)
+        # SC: O(1)
 
-            for i in range(start, end):
-                if arr[i] <= pivot_value:
-                    arr[i], arr[pivot_point] = arr[pivot_point], arr[i]
-                    pivot_point += 1
+        # we could use a heap to store the elmeents
+        # heapifying the array would require O(n) time
+        # Poping each element from heap would require O(log n)
+        # it will require O(n + k log n)
+        # SP: O(1)
+        # arr = [1, 2, 3, 4, 5] 
 
-            arr[pivot_point], arr[end] = arr[end], arr[pivot_point]
-            if arr[start] == arr[end] and k >= start and k <= end:
-                return arr[k]
+        heapq.heapify(nums)
 
-            if pivot_point == len(arr) - k:
-                return arr[pivot_point]
-            elif pivot_point < len(arr) - k:
-                return quickselect(pivot_point+1, end, arr, k)
-            else:
-                return quickselect(start, pivot_point-1, arr, k)
+        for _ in range(len(nums) - k):
+            heapq.heappop(nums)
 
-        return quickselect(0, len(nums)-1, nums, k)
+        return heapq.heappop(nums)
