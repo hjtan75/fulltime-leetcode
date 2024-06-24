95a485caa1cd4e0c8d320e95cc34bf7af3c785e5
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-06-24 21:11:27 +0100

    Time: 1394 ms (31.42%) | Memory: 16.7 MB (84.01%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -15,16 +15,17 @@ class Solution:
 
         # Create a dp array with row as summ and i as index
         half = sum(nums) // 2
-        dp = [[False for _ in range(half+1)] for _ in range(len(nums)+1)]
+        current_row = [False for _ in range(half+1)]
+        previous_row = [False for _ in range(half+1)]
 
-        dp[0][0] = True
-        # print(dp)
+        previous_row[0] = True
         for i in range(1, len(nums)+1):
             for j in range(half+1):
-                # print(i, j)
                 if j - nums[i-1] >= 0:
-                    dp[i][j] = dp[i-1][j] | dp[i-1][j-nums[i-1]]
+                    current_row[j] = previous_row[j] | previous_row[j-nums[i-1]]
                 else:
-                    dp[i][j] = dp[i-1][j]
+                    current_row[j] = previous_row[j]
 
-        return dp[len(nums)][half]
+            previous_row = current_row[:]
+
+        return previous_row[half]
