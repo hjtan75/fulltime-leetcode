0682d2bace7f7e99d823b5f60dfc0231432684b4
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-07-16 15:29:21 +0100

    Time: 37 ms (40.94%) | Memory: 16.6 MB (13.11%) - LeetSync

--- 468-validate-ip-address/validate-ip-address.py
+++ 468-validate-ip-address/validate-ip-address.py
@@ -1,45 +1,21 @@
 class Solution:
     def validIPAddress(self, queryIP: str) -> str:
-        dot_cnt = 0
-        sem_cnt = 0
+        def isIPv4(s):
+            try:
+                num = int(s)
+                return str(num) == s and num <= 255
+            except:
+                return False
 
-        if len(queryIP) > 39:
-            return 'Neither'
+        def isIPv6(s):
+            try:
+                return len(s) <= 4 and int(s, 16) >= 0
+            except:
+                return False
 
-        for c in queryIP:
-            if c == '.':
-                dot_cnt += 1
-            if c == ':':
-                sem_cnt += 1
+        if queryIP.count('.') == 3 and all(isIPv4(i) for i in queryIP.split('.')):
+            return "IPv4"
 
-        if dot_cnt == 3 and sem_cnt == 0:
-            classes = queryIP.split('.')
-            if len(classes) != 4:
-                return 'Neither'
-            for cla in classes:
-                if cla.isnumeric():
-                    if (len(cla) > 1 and cla[0] == '0') or len(cla) > 3:
-                        return 'Neither'
-
-                    num = int(cla)
-                    if num > 255:
-                        return 'Neither'
-                else:
-                    return 'Neither'
-            return 'IPv4'
-
-
-        if sem_cnt == 7 and dot_cnt == 0:
-            classes = queryIP.split(':')
-            if len(classes) != 8:
-                return 'Neither'
-            for cla in classes:
-                if len(cla) < 1 or len(cla) > 4:
-                    return 'Neither'
-                for c in cla:
-                    if c not in '0123456789' and c.lower() not in 'abcdef':
-                        return 'Neither'
-                
-            return 'IPv6'
-
+        if queryIP.count(':') == 7 and all(isIPv6(i) for i in queryIP.split(':')):
+            return "IPv6"
         return 'Neither'
