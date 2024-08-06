0fc72526a724db449fbe982ccf2e35385d478367
Tan Hong Jie <44732367+hjtan75@users.noreply.github.com>
2024-08-06 15:29:21 +0100

    Time: 3 ms (93.33%) | Memory: 8.6 MB (31.74%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        vector<int> arr;
-
         if(x < 0) return false;
 
+        long x_copy = x;
+        long x_reversed = 0;
+
         while(x > 0) {
-            int digit = x % 10;
+            x_reversed = (x_reversed * 10) + (x % 10);
             x = floor(x/10);
-            arr.push_back(digit);
         }
 
-        int l = 0;
-        int r = arr.size() - 1;
-
-        while(l < r) {
-            if(arr[l] != arr[r]) {
-                return false;
-            }
-            l++;
-            r--;
-        }
-
-        return true;
+        return x_copy == x_reversed;
     }
 };
